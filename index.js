a4c615db5c45ae12e6fbce5da0d33387bbc91622
Aaron <ztoplin@yahoo.com>
2022-10-24 15:20:00 -0400

    into fish without styling

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-console.log('I am working')
+const URL = 'https://www.fishwatch.gov/api/species'
 
-fetch('https://dadjokes.com/')
- .then(res => res.json())
- .then(data => {
+const topFishName = document.querySelector('#fish_name')
+const fishImage = document.querySelector('#fish_image')
+const fishDescription = document.querySelector('#fish_description')
+const fishNavList = document.querySelector('#fish_list')
+
+fetch('https://www.fishwatch.gov/api/species')
+ .then(response => response.json() )
+ .then(fetchedItems => {
+    console.log(fetchedItems)
+    displayInfo(fetchedItems[0])
+    fetchedItems.forEach( fetchedItem => {
+        const li = document.createElement('li')
+        li.textContent = fetchedItem['Species Name']
+        fishNavList.append(li) 
+    
+        li.addEventListener('click', (e) => {
+                e.preventDefault()
+                displayInfo(fetchedItem)
+                
+            })})
+
+        // li.addEventListener('click', (e) => {
+        //     e.preventDefault()
+        //     displayInfo(fetchedItem)
+            
+        // })
+    })
+
+function displayInfo(fishItem) {
+    topFishName.textContent = fishItem['Species Name']
+    fishImage.src = fishItem['Image Gallery'][0].src
+    fishDescription.textContent = fishItem['Physical Description']
+    .replace('<ul>', '').replace('</ul>', '').replace('<li>', '').replace('</li>','')
+}
+ 
+
+
+
+
